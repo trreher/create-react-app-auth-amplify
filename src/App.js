fa1d22f1800845fa7d844cd6ef444bf3e20fcfee
Tyler Reher <trreher@gmail.com>
2021-12-10 20:43:27 -0600

    Update App.js
    Update to use previously created Cognito configuration

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import Amplify, { Auth } from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-1:4c145053-8fbb-4ca6-b823-ee91510cc6d2',
+        
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-1',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-1_MQiQyLUwG',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '4qmfohfv0r7rkiuhepqcq3ecj',
+    }
+});
 
 class App extends Component {
   render() {
